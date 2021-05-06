9a4d5021ec5e33e4877bec34367250e01e897a2f
AxelV97 <axelskr97@gmail.com>
2021-05-06 03:26:56 -0500

    Ajustes en dependencia Swagger

--- TestingCore/Startup.cs
+++ TestingCore/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
-using Swashbuckle.Swagger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
